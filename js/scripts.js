bc8d293cb615b9a202051e7574bbf24dee80bc92
paramshah <paramdshah@gmail.com>
2015-12-19 15:57:34 -0800

    map location

--- js/scripts.js
+++ js/scripts.js
@@ -275,7 +275,7 @@
     var mapOptions = {
       // How zoomed in you want the map to start at (always required)
       zoom: 11, // The latitude and longitude to center the map (always required)
-      center: new google.maps.LatLng(40.6700, -73.9400), // New York           // How you would like to style the map. 
+      center: new google.maps.LatLng(39.2833, -76.6167), // New York           // How you would like to style the map. 
       // This is where you would paste any style found on Snazzy Maps.
       styles: [{
         "featureType": "water",
@@ -398,7 +398,7 @@
     var mapElement = document.getElementById('map'); // Create the Google Map using our element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions); // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(40.6700, -73.9400),
+      position: new google.maps.LatLng(39.3272987, -76.617777),
       map: map,
       icon: 'img/map-marker.png'
     });
